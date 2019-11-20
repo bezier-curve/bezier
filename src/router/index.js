6be7d40266876af62afed7a007f35982898a8534
zhenghouyan <zhenghouyan@360.net>
2019-11-20 21:30:21 +0800

    提交路由代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
-
+import bezier from '@/components/bezier/aaaa'
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'index',
-    component: Index
+    name: 'bezier',
+    component: bezier
   },
   {
     path: '/*',
@@ -16,7 +15,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/404.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/404.vue')
   }
 ]
 
