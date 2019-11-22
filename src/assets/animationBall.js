5c286605453fe68e797108b60342368f3bc58a59
zhenghouyan <zhenghouyan@360.net>
2019-11-22 21:13:09 +0800

    提交代码

--- src/assets/animationBall.js
+++ src/assets/animationBall.js
@@ -1,5 +1,5 @@
 class animationBall {
-    constructor(o) {
+    constructor(o,type) {
         this.x = o.x || 0,
             this.y = o.y || 0,
             this.color = o.color || '#0F0',
@@ -8,14 +8,48 @@ class animationBall {
             this.t = o.t || 0,
             this.loopIndex = o.loopIndex || 0
             this.oldLoopIndex = o.oldLoopIndex || 0
+            this.type = type||'ball',
+            this.img = o.img||null,
+            this.angle = 0
     }
-    drawBall(ctx) {
+    drawTo(ctx) {
+        switch (this.type) {
+            case 'ball':
+                this.drawBall(ctx)
+                break;
+            case "img":
+                this.drawImg(ctx)
+                break;
+            default:
+                this.drawBall(ctx)
+        }
+    }
+    drawImg(){
         if (!this.visible) { return }  // 不可见
         //画一个实心圆
+        ctx.save()
         ctx.beginPath()
+        ctx.translate(this.x, this.y);
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false) // 顺时针
         ctx.fillStyle = this.color;//填充颜色,默认是黑色
         ctx.fill()//画实心圆
+        ctx.restore()
+    }
+    drawBall(ctx,img) {
+        if (!this.visible) { return }  // 不可见
+        //画一个实心圆
+        ctx.save()
+        ctx.beginPath()
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle+1)
+        ctx.translate(-this.x, -this.y);
+        ctx.drawImage(img, this.x - 20, this.y - 20, 39, 38);
+        // ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false) // 顺时针
+        // ctx.fillStyle = this.color;//填充颜色,默认是黑色
+        ctx.fill()//画实心圆
+        ctx.restore()
+        
+        
     }
     update(m_x, m_y) {
         if (this.isSelect) {
